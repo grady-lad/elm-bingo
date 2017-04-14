ab31da01bb572ede533d027be35883ab7d068f8c
grady-lad <ogradymartin7@gmail.com>
2017-04-14 10:15:32 +0200

    Elm bingo intial commit

---
+++ Bingo.elm
@@ -0,0 +1,135 @@
+module Bingo exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+-- Model
+
+
+type alias Model =
+    { name : String, gameNumber : Int, entries : List Entry }
+
+
+type alias Entry =
+    { id : Int, phrase : String, points : Int, marked : Bool }
+
+
+initialModel : Model
+initialModel =
+    { name = "Martin"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+
+initialEntries : List Entry
+initialEntries =
+    [ Entry 1 "Future-Proof" 100 False
+    , Entry 2 "Doing Agile" 200 False
+    , Entry 3 "In the Cloud" 300 False
+    , Entry 4 "Rock Star Ninja" 400 False
+    ]
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NewGame ->
+            { model
+                | gameNumber = model.gameNumber + 1
+                , entries = initialEntries
+            }
+
+        Mark id ->
+            let
+                markEntry e =
+                    if e.id == id then
+                        { e | marked = not (e.marked) }
+                    else
+                        e
+            in
+                { model | entries = List.map markEntry model.entries }
+
+
+type Msg
+    = NewGame
+    | Mark Int
+
+
+
+-- View
+
+
+playerInfo : String -> Int -> String
+playerInfo name gameNumber =
+    name ++ " - Game " ++ (toString gameNumber)
+
+
+viewPlayer : String -> Int -> Html msg
+viewPlayer name gameNumber =
+    let
+        playInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "classy" ]
+            [ playInfoText ]
+
+
+viewHeader : String -> Html msg
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter : Html Msg
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org" ]
+            [ text "Powered By elm" ]
+        ]
+
+
+viewEntryItem : Entry -> Html Msg
+viewEntryItem entryItem =
+    li [ classList [ ( "marked", entryItem.marked ) ], onClick (Mark entryItem.id) ]
+        [ span [ class "phrase" ] [ text entryItem.phrase ]
+        , span [ class "points" ] [ text (toString entryItem.points) ]
+        ]
+
+
+viewEntryList : List Entry -> Html Msg
+viewEntryList entries =
+    let
+        listOfEntries =
+            List.map viewEntryItem entries
+    in
+        ul [] listOfEntries
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BING"
+        , viewPlayer model.name model.gameNumber
+        , viewEntryList model.entries
+        , button [ onClick NewGame ] [ text "New Game" ]
+        , div [ class "debug" ] [ text (toString model) ]
+        , viewFooter
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
